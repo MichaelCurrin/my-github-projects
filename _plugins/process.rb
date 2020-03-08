56cb39db27e64cf27dae9cd1dbfe734ac5bc5cab
Michael Currin <michael.currin@gmail.com>
2020-03-08 11:18:36 +0200

    style: Tabs to spaces

--- _plugins/process.rb
+++ _plugins/process.rb
@@ -1,45 +1,46 @@
 require_relative 'request'
+
 DEBUG = ENV['DEBUG']
 
 module GithubData
-	def self.get_gh_data
-		resp_data = Request.query
+  def self.get_gh_data
+    resp_data = Request.query
 
-		fetched_repos = resp_data['viewer']['repositories']['nodes']
+    fetched_repos = resp_data['viewer']['repositories']['nodes']
 
-		repos = {}
-		topics = Hash.new { |hash, key| hash[key] = {} }
+    repos = {}
+    topics = Hash.new { |hash, key| hash[key] = {} }
 
-		for fetched_repo in fetched_repos do
-			if DEBUG
-				puts "FETCHED #{fetched_repo["name"]}"
-      		end
+    for fetched_repo in fetched_repos do
+      if DEBUG
+        puts "FETCHED #{fetched_repo["name"]}"
+      end
 
-			repo_topics = fetched_repo["repositoryTopics"]["nodes"]
-			# TODO Use other attributes. For now just repo name.
-			topics_of_fetched_repo = repo_topics.map { |t| t["topic"]["name"] }
+      repo_topics = fetched_repo["repositoryTopics"]["nodes"]
+      # TODO Use other attributes. For now just repo name.
+      topics_of_fetched_repo = repo_topics.map { |t| t["topic"]["name"] }
 
-			# We can't use symbols here as Jekyll can't use symbols.
-			# A class or struct was not practical so an ad hoc hash is used here.
-			repo = {}
-			repo['name'] = fetched_repo["name"]
-			repo['created_at'] = fetched_repo["createdAt"]
-			repo['stars'] = fetched_repo["stargazers"]["totalCount"]
-			repo['topics'] = topics_of_fetched_repo
+      # We can't use symbols here as Jekyll can't use symbols.
+      # A class or struct was not practical so an ad hoc hash is used here.
+      repo = {}
+      repo['name'] = fetched_repo["name"]
+      repo['created_at'] = fetched_repo["createdAt"]
+      repo['stars'] = fetched_repo["stargazers"]["totalCount"]
+      repo['topics'] = topics_of_fetched_repo
 
-			repos[repo['name']] = repo
+      repos[repo['name']] = repo
 
-			for topic in topics_of_fetched_repo do
-			    if DEBUG
-				    puts "  TOPIC #{topic}"
-        		end
+      for topic in topics_of_fetched_repo do
+        if DEBUG
+          puts "  TOPIC #{topic}"
+        end
 
-				stored_topics = topics[topic]
-				stored_topics[repo["name"]] = repo
-				topics[topic] = stored_topics
-			end
-		end
+        stored_topics = topics[topic]
+        stored_topics[repo["name"]] = repo
+        topics[topic] = stored_topics
+      end
+    end
 
-		[repos, topics]
-	end
+    [repos, topics]
+  end
 end
