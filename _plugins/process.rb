89c0d3dd884b64aa48c33cea8e4e5dc369d7bdb2
Michael Currin <michael.currin@gmail.com>
2020-03-08 12:48:02 +0200

    style: Update quotes

--- _plugins/process.rb
+++ _plugins/process.rb
@@ -13,16 +13,16 @@ module GithubData
         puts "FETCHED #{fetched_repo["name"]}"
       end
 
-      repo_topics = fetched_repo["repositoryTopics"]["nodes"]
+      repo_topics = fetched_repo['repositoryTopics']['nodes']
       # TODO Use other attributes. For now just repo name.
-      topics_of_fetched_repo = repo_topics.map { |t| t["topic"]["name"] }
+      topics_of_fetched_repo = repo_topics.map { |t| t['topic']['name'] }
 
       # We can't use symbols here as Jekyll can't use symbols.
       # A class or struct was not practical so an ad hoc hash is used here.
       repo = {}
-      repo['name'] = fetched_repo["name"]
-      repo['created_at'] = fetched_repo["createdAt"]
-      repo['stars'] = fetched_repo["stargazers"]["totalCount"]
+      repo['name'] = fetched_repo['name']
+      repo['created_at'] = fetched_repo['createdAt']
+      repo['stars'] = fetched_repo['stargazers']['totalCount']
       repo['topics'] = topics_of_fetched_repo
 
       repos[repo['name']] = repo
@@ -33,7 +33,7 @@ module GithubData
         end
 
         stored_topics = topics[topic]
-        stored_topics[repo["name"]] = repo
+        stored_topics[repo['name']] = repo
         topics[topic] = stored_topics
       end
     end
