1725d66be632d331615016dc996ea39e512f13bc
Michael Currin <michael.currin@gmail.com>
2020-03-08 13:00:22 +0200

    refactor: Add function

--- _plugins/process.rb
+++ _plugins/process.rb
@@ -4,8 +4,25 @@ DEBUG = ENV['DEBUG']
 
 module GithubData
 
+  def self.parse_repo(fetched_repo)
+    # We can't use symbols here as Jekyll can't use symbols.
+    # A class or struct was not practical so an ad hoc hash is used here.
+    repo = {}
+    repo['name'] = fetched_repo['name']
+    repo['created_at'] = fetched_repo['createdAt']
+    repo['stars'] = fetched_repo['stargazers']['totalCount']
+
+    fetched_topics = fetched_repo['repositoryTopics']['nodes']
+    # List of topic names.
+    repo['topics'] = fetched_topics.map { |t| t['topic']['name'] }
+
+    repo
+  end
+
   def self.process_repos(fetched_repos)
     repos = {}
+    # Structure where key is topic name and value is a hash with key as repo name and
+    # value as hash of repo attributes.
     topics = Hash.new { |hash, key| hash[key] = {} }
 
     for fetched_repo in fetched_repos do
@@ -13,25 +30,19 @@ module GithubData
         puts "FETCHED #{fetched_repo["name"]}"
       end
 
+      repo = self.parse_repo(fetched_repo)
+      repos[repo['name']] = repo
+
       repo_topics = fetched_repo['repositoryTopics']['nodes']
       # TODO Use other attributes. For now just repo name.
       topics_of_fetched_repo = repo_topics.map { |t| t['topic']['name'] }
-
-      # We can't use symbols here as Jekyll can't use symbols.
-      # A class or struct was not practical so an ad hoc hash is used here.
-      repo = {}
-      repo['name'] = fetched_repo['name']
-      repo['created_at'] = fetched_repo['createdAt']
-      repo['stars'] = fetched_repo['stargazers']['totalCount']
-      repo['topics'] = topics_of_fetched_repo
-
-      repos[repo['name']] = repo
 
       for topic in topics_of_fetched_repo do
         if DEBUG
           puts "  TOPIC #{topic}"
         end
 
+        # TODO: Can this be refactored to one line?
         stored_topics = topics[topic]
         stored_topics[repo['name']] = repo
         topics[topic] = stored_topics
