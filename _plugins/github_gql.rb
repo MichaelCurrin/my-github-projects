f3f4033762a347a06446aaf5c2d328be0bbc9856
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-19 16:22:57 +0200

    refactor: Update github_gql.rb

--- _plugins/github_gql.rb
+++ _plugins/github_gql.rb
@@ -5,25 +5,32 @@ module GitHubGQL
   QUERY_PATH = "repos_with_topics.gql"
 
   class Generator < Jekyll::Generator
-    def generate(site)
-      token = ENV["GITHUB_TOKEN"]
-      disable_gql = ENV["DISABLE_GQL"]
-      debug = ENV["DEBUG"]
+    def read_env()
+      @disable_gql = ENV["DISABLE_GQL"]
+      @debug = ENV["DEBUG"]
 
-      unless token
+      @token = ENV["GITHUB_TOKEN"]
+      unless @token
         raise "GitHub API token must be set"
       end
+    end
 
-      if disable_gql
-        repos = {}
-        topics = {}
+    def query
+      if @disable_gql
+        @repos = {}
+        @topics = {}
       else
-        gh_api = Process::GitHubAPI.new(token, QUERY_PATH, debug)
-        repos, topics = gh_api.get_gh_data
+        gh_api = Process::GitHubAPI.new(@token, QUERY_PATH, @debug)
+        @repos, @topics = gh_api.get_gh_data
       end
+    end
 
-      site.data["all_repos"] = repos
-      site.data["all_topics"] = topics
+    def generate(site)
+      self.read_env
+      self.query
+
+      site.data["all_repos"] = @repos
+      site.data["all_topics"] = @topics
     end
   end
 end
